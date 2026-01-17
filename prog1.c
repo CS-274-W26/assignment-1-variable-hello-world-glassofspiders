7e90ceb95fe5fe39f7186d29297817aa9a097595
Michael Savage <savagemi@flip1.engr.oregonstate.edu>
2026-01-16 17:38:07 -0800

    Completed the assignment.

--- prog1.c
+++ prog1.c
@@ -1,11 +1,26 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdlib.h> // Necessary for rand()/ srand()
+#include <time.h> // Necessary for time()
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+
+    // Random number seed    
+	srand(time(NULL));
+
+    // Generates random number 0-10
+	int num = rand() % 11;
+
+    // Selects phrase based off random number (0-10) generated
+	if(num < 5){
+
+        printf("Eat more beef, kick less cats");
+    }else if(num < 10){
+
+        printf("FRODO LIVES");
+    }else if(num == 10) {
+
+        printf("Larn is the best roguelike");
+    }
+
+return 0;
 }
